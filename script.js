94c5ea5d6d15f952a560ffe6f28b74595e7bb5f4
Matthew David Wasylko <matthew.wasylko@gmail.com>
2025-11-03 20:28:42 +0000

    feat: MDW-0000: creates web_audio_amp
    Signed-off-by: Matthew David Wasylko <matthew.wasylko@gmail.com>

---
+++ script.js
@@ -0,0 +1,51 @@
+const volume = document.getElementById("volume");
+const bass = document.getElementById("bass");
+const mid = document.getElementById("mid");
+const treble = document.getElementById("treble");
+const visualiser = document.getElementById("visualiser");
+
+const context = new AudioContext();
+const analyserNode = new AnalyserNode(context, { fftSize: 256 });
+
+function getGuitar() {
+  return navigator.mediaDevices.getUserMedia({
+    audio: {
+      echoCancellation: false,
+      autoGainControl: false,
+      noiseSuppresion: false,
+      latency: 0,
+    },
+  });
+}
+
+async function setupContext() {
+  const guitar = await getGuitar();
+  if (context.state === "suspended") {
+    await context.resume();
+  }
+  const source = context.createMediaStreamSource(guitar);
+  source.connect(analyserNode).connect(context.destination);
+}
+
+function drawVisualiser() {
+  requestAnimationFrame(drawVisualiser);
+
+  const bufferLength = analyserNode.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  analyserNode.getByteFrequencyData(dataArray);
+  const width = visualiser.width;
+  const height = visualiser.height;
+  const barWidth = width / bufferLength;
+  const canvasContext = visualiser.getContext("2d");
+  canvasContext.clearRect(0, 0, width, height);
+  dataArray.forEach((item, index) => {
+    const y = ((item / 255) * height) / 2;
+    const x = barWidth * index;
+
+    canvasContext.fillStyle = `hsl(${(y / height) * 400}, 100%, 50%)`;
+    canvasContext.fillRect(x, height - y, barWidth, y);
+  });
+}
+
+setupContext();
+drawVisualiser();
