35639868e84ca5e5e97191c84e0b7f242b9d33b7
Matthew David Wasylko <matthew.wasylko@gmail.com>
2025-11-03 20:48:06 +0000

    feat: MDW-0000: adds EQ
    Signed-off-by: Matthew David Wasylko <matthew.wasylko@gmail.com>

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ const visualiser = document.getElementById("visualiser");
 
 const context = new AudioContext();
 const analyserNode = new AnalyserNode(context, { fftSize: 128 });
+const gainNode = new GainNode(context, { gain: volume.value });
+const bassEQ = new BiquadFilterNode(context, {
+  type: "lowshelf",
+  frequency: 500,
+  gain: bass.value,
+});
+const midEQ = new BiquadFilterNode(context, {
+  type: "peaking",
+  Q: Math.SQRT1_2,
+  frequency: 1500,
+  gain: mid.value,
+});
+const trebleEQ = new BiquadFilterNode(context, {
+  type: "highshelf",
+  frequency: 3000,
+  gain: treble.value,
+});
 
 function getGuitar() {
   return navigator.mediaDevices.getUserMedia({
@@ -18,13 +35,42 @@ function getGuitar() {
   });
 }
 
+function setupEventListeners() {
+  window.addEventListener("resize", resize);
+  volume.addEventListener("input", (e) => {
+    const value = parseFloat(e.target.value);
+    gainNode.gain.setTargetAtTime(value, context.currentTime, 0.01);
+  });
+
+  bass.addEventListener("input", (e) => {
+    const value = parseFloat(e.target.value);
+    bassEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
+  });
+
+  mid.addEventListener("input", (e) => {
+    const value = parseFloat(e.target.value);
+    midEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
+  });
+
+  treble.addEventListener("input", (e) => {
+    const value = parseFloat(e.target.value);
+    trebleEQ.gain.setTargetAtTime(value, context.currentTime, 0.01);
+  });
+}
+
 async function setupContext() {
   const guitar = await getGuitar();
   if (context.state === "suspended") {
     await context.resume();
   }
   const source = context.createMediaStreamSource(guitar);
-  source.connect(analyserNode).connect(context.destination);
+  source
+    .connect(bassEQ)
+    .connect(midEQ)
+    .connect(trebleEQ)
+    .connect(gainNode)
+    .connect(analyserNode)
+    .connect(context.destination);
 }
 
 function drawVisualiser() {
@@ -39,7 +85,7 @@ function drawVisualiser() {
   const canvasContext = visualiser.getContext("2d");
   canvasContext.clearRect(0, 0, width, height);
   dataArray.forEach((item, index) => {
-    const y = ((item / 255) * height) / 2;
+    const y = (item / 255) * height;
     const x = barWidth * index;
 
     canvasContext.fillStyle = `hsl(${(y / height) * 400}, 100%, 50%)`;
@@ -53,5 +99,6 @@ function resize() {
 }
 
 resize();
+setupEventListeners();
 setupContext();
 drawVisualiser();
